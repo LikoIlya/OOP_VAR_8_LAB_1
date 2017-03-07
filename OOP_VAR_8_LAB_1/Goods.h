afda5fe4ff8888fcae0f53381360ffaf22ef7014
Ilya Likhoshva <ilyaliko64@gmail.com>
2017-03-07 12:28:21 +0200

    udated comments

--- OOP_VAR_8_LAB_1/Goods.h
+++ OOP_VAR_8_LAB_1/Goods.h
@@ -16,22 +16,28 @@ private:
 	int _count;
 	Provider _provider;
 public:
+	//Constructors
 	Goods();
 	Goods(std::string GoodsName);
 	Goods(std::string GoodsName, int Price);
 	Goods(std::string GoodsName, int Price, int Count);
 	Goods(std::string GoodsName, int Price, int Count, Provider GoodsProvider);
 	Goods(const Goods &src);
+	//Destrucrtor
 	~Goods();
+	//Getters
 	std::string GetName() { return _name_of_goods; }
 	int GetPrice() { return _price_of_goods; }
 	int GetCount() { return _count; }
 	Provider GetProvider() { return _provider; }
-	void Put();
-	void PutFull();
+	//Setters
 	Goods& SetName(std::string GoodsName);
 	Goods& SetPrice(int Price);
 	Goods& SetCount(int Count);
 	Goods& SetProvider(Provider GoodsProvider);
+	//Methods
+	void Put();
+	void PutFull();
+	
 };
 
--- OOP_VAR_8_LAB_1/Provider.h
+++ OOP_VAR_8_LAB_1/Provider.h
@@ -14,19 +14,24 @@ private:
 	std::string _country_of_registration;
 	std::string _phone_number;
 public:
+	//Constructors
 	Provider();
 	Provider(std::string CompanyName);
 	Provider(std::string CompanyName, std::string RegistrationCountry);
 	Provider(std::string CompanyName, std::string RegistrationCountry, std::string Phone);
 	Provider(const Provider &src);
+	//Destrucrtor
 	~Provider();
+	//Getters
 	std::string GetName() { return _name_of_company; }
 	std::string GetCountry() { return _country_of_registration; }
 	std::string GetPhone() { return _phone_number; }
-	void Put();
-	void PutFull();
+	//Setters
 	Provider& SetName(std::string CompanyName);
 	Provider& SetCountry(std::string RegistrationCountry);
 	Provider& SetPhone(std::string Phone);
+	//Methods
+	void Put();
+	void PutFull();
 };
 
